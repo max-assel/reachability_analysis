99d31918d6dc63f3292d9491ecccf52953ea1cbc
Maxwell Asselmeier <asselmeier.max@gmail.com>
2026-02-05 15:46:22 -0500

    using go2

--- launch/reachability_analysis.launch.py
+++ launch/reachability_analysis.launch.py
@@ -22,8 +22,8 @@ def generate_launch_description():
     # Package Directories #
     #######################
 
-    anymal_description_path = get_package_share_directory("anymal_description")
-    # go2_description_path = get_package_share_directory("go2_description")
+    # anymal_description_path = get_package_share_directory("anymal_description")
+    go2_description_path = get_package_share_directory("go2_description")
     
     # mmp_quadruped_path = get_package_share_directory("mmp_quadruped")
     reachability_analysis_path = get_package_share_directory("reachability_analysis")
@@ -31,8 +31,8 @@ def generate_launch_description():
     ####################
     # Launch Arguments #
     ####################
-    urdfFile = os.path.join(anymal_description_path, "urdf/anymal.urdf")
-    # urdfFile = os.path.join(go2_description_path, "urdf/go2_description.urdf")
+    # urdfFile = os.path.join(anymal_description_path, "urdf/anymal.urdf")
+    urdfFile = os.path.join(go2_description_path, "urdf/go2_description.urdf")
 
     taskFile = os.path.join(reachability_analysis_path, "config/task.info")
     frameFile = os.path.join(reachability_analysis_path, "config/frame_declaration.info")
